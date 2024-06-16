92df49a4425599f648b5c16316276b8756a6d4ba
timo-pedersen <78095279+timo-pedersen@users.noreply.github.com>
2024-06-16 07:36:26 +0200

    Create winterm.js
    Some settings for winterm

---
+++ WinTerm/winterm.js
@@ -0,0 +1,23 @@
+"defaults": 
+{
+    "bellStyle": "taskbar",
+    "colorScheme": "Campbell",
+    "font": 
+    {
+        "face": "0xProto Nerd Font",
+        "size": 11.0
+    },
+    "opacity": 90,
+    "padding": "2",
+    "startingDirectory": "%USERPROFILE%",
+    "useAtlasEngine": true
+},
+
+# Msys2 UCRT64
+{
+    "commandline": "C:/msys64/msys2_shell.cmd -use-full-path -defterm -here -no-start -ucrt64 -shell bash",
+    "guid": "{17da3cac-b318-431e-8a3e-7fcdefe6d114}",
+    "hidden": false,
+    "icon": "C:/msys64/ucrt64.ico",
+    "name": "MSYS2 - ucrt64"
+}
